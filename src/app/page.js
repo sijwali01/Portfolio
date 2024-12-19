5d371c654950d220e802e6562485ec538edf6c0c
Manish Sijwali <152587691+sijwali01@users.noreply.github.com>
2024-12-20 03:03:04 +0530

    added links

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
 export default function page() {
-  return <h1> Home Page</h1>
+  return(
+    <>
+    <h1 className='text-3xl font-bold underline'> Home Page</h1>
+    
+    <button> <Link href="/about">About</Link></button>
+    <button> <Link href="/profile">Profile</Link></button>
+
+    </>
+
+  ) 
+  
 };
