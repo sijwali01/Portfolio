44ddb3f95bbce6065b2c64c16a0933306dc505af
Manish Sijwali <152587691+sijwali01@users.noreply.github.com>
2024-12-20 03:22:33 +0530

    ui changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default function page() {
-  return(
+  return (
     <>
-    <h1 className='text-3xl font-bold underline'> Home Page</h1>
-    
-    <button> <Link href="/about">About</Link></button>
-    <button> <Link href="/profile">Profile</Link></button>
-
+      <h1 className="text-3xl font-bold underline m-4"> Home Page</h1>
+      <div className="m-2">
+        <button  className="rounded-md m-2 bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >
+          {" "}
+          <Link href="/about">About</Link>
+        </button>
+        <button  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          {" "}
+          <Link href="/profile">Profile</Link>
+        </button>
+      </div>
     </>
-
-  ) 
-  
-};
+  );
+}
